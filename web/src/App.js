00ca216ee430138f96f7bfcc8fa35171e56a3691
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-25 16:53:23 +0900

    Update App.js
    /product/제품명 경로 추가

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,7 @@
 // 페이지 이동 목적 모듈 import
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
 import ProductListPage from "./pages/ProductListPage";
 import TestPage from "./pages/TestPage";
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/test" element={<TestPage />} />
+        <Route path="/product/:productName" element={<ProductDetailPage />} />
         <Route path="/product/:category" element={<ProductListPage />} />
       </Routes>
     </BrowserRouter>
