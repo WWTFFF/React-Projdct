4e52a13a6031a1cbe749f5a70b029b754f237cc5
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-25 16:32:21 +0900

    Update App.js
    /test 경로 추가

--- web/src/App.js
+++ web/src/App.js
@@ -2,12 +2,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import ProductListPage from "./pages/ProductListPage";
+import TestPage from "./pages/TestPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/test" element={<TestPage />} />
         <Route path="/product/:category" element={<ProductListPage />} />
       </Routes>
     </BrowserRouter>
