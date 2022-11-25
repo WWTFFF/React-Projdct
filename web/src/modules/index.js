f9b38f23a1a0fbfd0bfadd02a750b2fb40949ceb
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-25 16:32:39 +0900

    Update index.js
    ProductRecommend 오타 수정

--- web/src/modules/index.js
+++ web/src/modules/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import productRecommend from "./productRecommend";
+import productRecommend from "./ProductRecommend";
 
 const rootReducer = combineReducers({
   productRecommend,
