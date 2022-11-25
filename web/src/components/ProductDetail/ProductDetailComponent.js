d0d9dfc511ec6a42fd9d8e3a3879e9df0df827f7
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-25 16:53:45 +0900

    Update ProductDetailComponent.js
    제품 컴포넌트 추가

--- web/src/components/ProductDetail/ProductDetailComponent.js
+++ web/src/components/ProductDetail/ProductDetailComponent.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-import { Badge, Button, Flex, Image, Text } from "@aws-amplify/ui-react";
+import { Flex, Image, Text } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import ProductDetailBuyButton from "./ProductDetailBuyButton";
+import ProductDetailInformation from "./ProductDetailInformation";
 
 const ProductDetailComponent = () => {
   // TODO 해당 제품 로딩하는 메소드 작성할 것
@@ -24,45 +26,11 @@ const ProductDetailComponent = () => {
         </Flex>
         {/* 우측 제품 설명 */}
         <Flex direction="column">
-          {/* 제목 */}
-          <Text fontSize="1.5rem">{dummyData.ProductName}</Text>
-          {/* 가격 표시 */}
-          <Flex direction="column" fontWeight="bold">
-            <Flex fontSize="1.8rem">
-              <Text color="rgb(250, 98, 47)">
-                {dummyData.Discount * 100 + "%"}
-              </Text>
-              <Text>
-                {(dummyData.Price * (1 - dummyData.Discount)).toLocaleString(
-                  "kr"
-                ) + "원"}
-              </Text>
-            </Flex>
-            <Text color="rgb(181, 181, 181)" textDecoration="line-through">
-              {dummyData.Price.toLocaleString("kr") + "원"}
-            </Text>
-          </Flex>
+          <ProductDetailInformation dummyData={dummyData} />
+
+          {/* 구매 버튼 표시 */}
+          <ProductDetailBuyButton />
         </Flex>
-      </Flex>
-      {/* 구매 정보 표시 */}
-      <Flex justifyContent="right">
-        <Flex direction="column" width="100%" textAlign="right">
-          <Text>총 상품 금액: {123}</Text>
-          <Flex alignItems="center" justifyContent="right">
-            <Badge backgroundColor="rgb(255, 191, 0)" color="white">
-              적립
-            </Badge>
-            <Text>로그인 후, 할인 및 적립 혜택 제공</Text>
-          </Flex>
-        </Flex>
-      </Flex>
-      {/* 구매 버튼 표시 */}
-      <Flex justifyContent="right" height="5rem">
-        <Button></Button>
-        <Button></Button>
-        <Button backgroundColor="rgb(95, 0, 128)" width="30rem" color="white">
-          장바구니 담기
-        </Button>
       </Flex>
     </Flex>
   );
