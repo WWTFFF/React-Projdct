7b9acae849bdc1ad80877ac146ec6a741f30b41f
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-28 14:44:42 +0900

    Update CategoryProductAPI.js
    오타 수정

--- web/src/api/CategoryProductAPI.js
+++ web/src/api/CategoryProductAPI.js
@@ -1,5 +1,6 @@
 // api 호출을 위한 모듈
 import axios from "axios";
+
 import { API_CATEGORY_PRODUCT } from "../templates/APITemplate";
 
 // 상품 추천
@@ -7,6 +8,7 @@ import { API_CATEGORY_PRODUCT } from "../templates/APITemplate";
 // GET ~/category/:category
 // 카테고리 상품 데이터를 가져오는 API
 
-export const getCategoryProductAPI = ({ url, category }) => {
-  return axios.get(`${url}${API_CATEGORY_PRODUCT}/${category}}`);
+export const getCategoryProductAPI = (url, category) => {
+  console.log(`${url}${API_CATEGORY_PRODUCT}/${category}`);
+  return axios.get(`${url}${API_CATEGORY_PRODUCT}/${category}`);
 };
