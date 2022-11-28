bd39d7c09d10a9518a3296dc66d908e101c8a4d5
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-28 17:48:23 +0900

    Update APITemplate.js
    상품 상세 정보 요청 URI 추가

--- web/src/templates/APITemplate.js
+++ web/src/templates/APITemplate.js
@@ -3,3 +3,6 @@ export const API_PRODUCT_RECOMMEND = `/product/recommend`;
 
 // GET - 카테고리 상품 목록
 export const API_CATEGORY_PRODUCT = `/product/list/category`;
+
+// GET - 상품 상세 정보
+export const API_PRODUCT_DETAIL = `/product/list/search`;
