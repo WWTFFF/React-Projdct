ce9e24e1f46d55d8c1b9632c2d1f5a4d69057adf
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-28 13:42:20 +0900

    Update CategoryData.js
    카테고리 명칭 변경

--- web/src/data/CategoryData.js
+++ web/src/data/CategoryData.js
@@ -1,6 +1,6 @@
 export const CategoryDict = {
-  fruit: { kname: "채소", link: "/product/fruit" },
-  meat: { kname: "육류", link: "/product/meat" },
+  fruit: { kname: "과일", link: "/category/과일" },
+  meat: { kname: "채소", link: "/category/채소" },
   meat2: { kname: "육류2", link: "/product/meat2" },
   meat3: { kname: "육류2", link: "/product/meat3" },
 };
