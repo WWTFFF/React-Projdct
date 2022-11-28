76f274cbfb702d5b445eb504c1de652df31abdd9
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-28 18:21:05 +0900

    Update index.js
    index에 productDetail 상태관리 추가

--- web/src/modules/index.js
+++ web/src/modules/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import productRecommend from "./ProductRecommend";
 import categoryProduct from "./CategoryProduct";
+import productDetail from "./ProductDetail";
 
 const rootReducer = combineReducers({
   productRecommend,
   categoryProduct,
+  productDetail,
 });
 
 export default rootReducer;
