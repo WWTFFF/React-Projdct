4f5b86cc6601e717aff68ae1160c9a0761d2a384
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-28 18:21:43 +0900

    Update ProductDetailComponent.js
    상품 상세 컴포넌트 추가

--- web/src/components/ProductDetail/ProductDetailComponent.js
+++ web/src/components/ProductDetail/ProductDetailComponent.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { Flex, Image } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import ProductDetailBuyButton from "./ProductDetailBuyButton";
 import ProductDetailInformation from "./ProductDetailInformation";
+import { useDispatch, useSelector } from "react-redux";
+import { getProductDetail } from "../../modules/ProductDetail";
+import Loading from "../Common/Loading";
 
 const ProductDetailComponent = ({ productName }) => {
   // TODO 해당 제품 로딩하는 메소드 작성할 것
+  // dispatch
+  const dispatch = useDispatch();
+
+  // useSelector
+  const product = useSelector((state) => state.productDetail.product);
+  const loading = useSelector(
+    (state) => state.productDetail.loading.GET_PRODUCT_DETAIL
+  );
+
+  useEffect(() => {
+    dispatch(getProductDetail(productName));
+  }, []);
 
   const dummyData = {
     ProductName: "[브룩클린688] 호주산 목초육 치마살 구이용 300g (냉장)",
@@ -16,23 +31,24 @@ const ProductDetailComponent = ({ productName }) => {
 
   return (
     <Flex direction="column">
-      <Flex direction="row" justifyContent="center" columnGap="5rem">
-        <Flex>
-          {/* 좌측 제품 사진 */}
-          <Image
-            src="https://img-cf.kurly.com/shop/data/goods/165303917855l0.jpeg"
-            width="430px"
-            height="552px"
-          />
-        </Flex>
-        {/* 우측 제품 설명 */}
-        <Flex direction="column">
-          <ProductDetailInformation dummyData={dummyData} />
+      {loading ? (
+        <Loading />
+      ) : (
+        <Flex direction="row" justifyContent="center" columnGap="5rem">
+          {console.log(product)}
+          <Flex>
+            {/* 좌측 제품 사진 */}
+            <Image src={product.img_url} width="430px" height="552px" />
+          </Flex>
+          {/* 우측 제품 설명 */}
+          <Flex direction="column">
+            <ProductDetailInformation product={product} />
 
-          {/* 구매 버튼 표시 */}
-          <ProductDetailBuyButton />
+            {/* 구매 버튼 표시 */}
+            <ProductDetailBuyButton />
+          </Flex>
         </Flex>
-      </Flex>
+      )}
     </Flex>
   );
 };
