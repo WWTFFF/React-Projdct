31b39b29b0e81878d532a18c2d2312cefe74ef07
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-28 13:42:30 +0900

    Create CategoryProductAPI.js
    카테고리 상품 데이터를 가져오는 API 생성

---
+++ web/src/api/CategoryProductAPI.js
@@ -0,0 +1,12 @@
+// api 호출을 위한 모듈
+import axios from "axios";
+import { API_CATEGORY_PRODUCT } from "../templates/APITemplate";
+
+// 상품 추천
+
+// GET ~/category/:category
+// 카테고리 상품 데이터를 가져오는 API
+
+export const getCategoryProductAPI = ({ url, category }) => {
+  return axios.get(`${url}${API_CATEGORY_PRODUCT}/${category}}`);
+};
