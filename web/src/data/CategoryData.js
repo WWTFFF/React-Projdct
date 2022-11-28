e3654777dabaef4ca233986cd951d82d0b551a74
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-28 14:44:50 +0900

    Update CategoryData.js
    카테고리 URL 형식 변경

--- web/src/data/CategoryData.js
+++ web/src/data/CategoryData.js
@@ -1,6 +1,6 @@
 export const CategoryDict = {
-  fruit: { kname: "과일", link: "/category/과일" },
-  meat: { kname: "채소", link: "/category/채소" },
+  fruit: { kname: "과일", link: "/category/fruit" },
+  vegetable: { kname: "채소", link: "/category/vegetable" },
   meat2: { kname: "육류2", link: "/product/meat2" },
   meat3: { kname: "육류2", link: "/product/meat3" },
 };
