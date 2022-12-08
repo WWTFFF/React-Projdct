33715ddce74d08c5028677011b7db68a0aeab23a
qpdh <74190329+qpdh@users.noreply.github.com>
2022-12-08 15:32:06 +0900

    Update ProductDetailComponent.js
    제품 상세정보 컴포넌트 제품 데이터를 받아와서 처리로 변경

--- web/src/components/ProductDetail/ProductDetailComponent.js
+++ web/src/components/ProductDetail/ProductDetailComponent.js
@@ -4,29 +4,30 @@ import { Flex, Image, Text } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import ProductDetailBuyButton from "./ProductDetailBuyButton";
 import ProductDetailInformation from "./ProductDetailInformation";
+import { useSelector } from "react-redux";
 
 const ProductDetailComponent = () => {
   // TODO 해당 제품 로딩하는 메소드 작성할 것
+
+  // useSelector
+  const product = useSelector((state) => state.productDetail.product);
 
   const dummyData = {
     ProductName: "[브룩클린688] 호주산 목초육 치마살 구이용 300g (냉장)",
     Price: 15500,
     Discount: 0.25,
   };
+
   return (
     <Flex direction="column">
       <Flex direction="row" justifyContent="center" columnGap="5rem">
         <Flex>
           {/* 좌측 제품 사진 */}
-          <Image
-            src="https://img-cf.kurly.com/shop/data/goods/165303917855l0.jpeg"
-            width="430px"
-            height="552px"
-          />
+          <Image src={product.img_url} width="430px" height="552px" />
         </Flex>
         {/* 우측 제품 설명 */}
         <Flex direction="column">
-          <ProductDetailInformation dummyData={dummyData} />
+          <ProductDetailInformation product={product} />
 
           {/* 구매 버튼 표시 */}
           <ProductDetailBuyButton />
