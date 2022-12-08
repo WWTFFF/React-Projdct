0c2ec7c5e47a2f26af96c37587ad8eef8c8576b0
qpdh <74190329+qpdh@users.noreply.github.com>
2022-12-08 15:33:38 +0900

    Update APITemplate.js
    API 템플릿에 상품 검색 URI추가

--- web/src/templates/APITemplate.js
+++ web/src/templates/APITemplate.js
@@ -3,3 +3,6 @@ export const API_PRODUCT_RECOMMEND = `/product/recommend`;
 
 // GET - 카테고리 상품 목록
 export const API_CATEGORY_PRODUCT = `/product/list/category`;
+
+// GET - 상품 검색
+export const API_SEARCH_PRODUCT = `/2013-01-01/search`;
