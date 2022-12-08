5510cc5a3a8be0cf64b4e2b433b76dd554ea988a
qpdh <74190329+qpdh@users.noreply.github.com>
2022-12-08 15:33:28 +0900

    Update URLTemplate.js
    URL 템플릿에 검색 URL 추가

--- web/src/templates/URLTemplate.js
+++ web/src/templates/URLTemplate.js
@@ -1,3 +1,7 @@
 export const API_GATE_WAY_URL =
   "https://st6b5nlr52.execute-api.ap-northeast-2.amazonaws.com";
+
 export const DJANGO_URL = "http://127.0.0.1:8000";
+
+export const SEARCH_URL =
+  "https://search-foodsearch-oogrquamn2b2gi4xsrmrqnol5i.ap-northeast-2.cloudsearch.amazonaws.com/";
