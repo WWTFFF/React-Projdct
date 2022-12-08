7de0bf8e98bab1bb4510e3cdc32a7cea2c487773
qpdh <74190329+qpdh@users.noreply.github.com>
2022-12-08 15:33:20 +0900

    Update index.js
    상품 검색, 상품 상세 상태관리 추가

--- web/src/modules/index.js
+++ web/src/modules/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from "redux";
 import productRecommend from "./ProductRecommend";
 import categoryProduct from "./CategoryProduct";
+import searchProduct from "./SearchProduct";
+import productDetail from "./ProductDetail"
 
 const rootReducer = combineReducers({
   productRecommend,
   categoryProduct,
+  searchProduct,
+  productDetail,
 });
 
 export default rootReducer;
